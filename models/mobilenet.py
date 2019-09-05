550a5320b297815dc3f0bae9d4f4de7ea898447b
William C Grisaitis <wgrisaitis@gmail.com>
2019-09-04 20:42:24 -0400

    Make MobileNet layer construction a bit more pythonic

--- models/mobilenet.py
+++ models/mobilenet.py
@@ -37,8 +37,10 @@ class MobileNet(nn.Module):
     def _make_layers(self, in_planes):
         layers = []
         for x in self.cfg:
-            out_planes = x if isinstance(x, int) else x[0]
-            stride = 1 if isinstance(x, int) else x[1]
+            try:
+                out_planes, stride = x
+            except TypeError:
+                out_planes, stride = x, 1
             layers.append(Block(in_planes, out_planes, stride))
             in_planes = out_planes
         return nn.Sequential(*layers)
